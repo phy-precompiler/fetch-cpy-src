3ed1c011c8a92fbba88f53e2aaead72cae444855
Alfred DU <ihearmywords@hotmail.com>
2025-05-06 13:57:59 +0800

    download github files & directory

--- src/phy_cpydeps_update/_downloader.py
+++ src/phy_cpydeps_update/_downloader.py
@@ -1,25 +1,54 @@
 """ download files of cpython repository """
 # imports
 from pathlib import Path
+import time
+
 from github import Github
 
-# constants
+
+# shared constansts & variables
 CPYTHON_REPO_OWNER = 'python'
 CPYTHON_REPO_NAME = 'cpython'
+
+gh = Github()
+repo = gh.get_repo(f'{CPYTHON_REPO_OWNER}/{CPYTHON_REPO_NAME}')
 
 
 def _download_cpython_file(path: str, tag: str, target_dir: Path) -> Path:
     """ download file from cpython repository """
-    gh = Github()
-    repo = gh.get_repo(f'{CPYTHON_REPO_OWNER}/{CPYTHON_REPO_NAME}')
-
     # if the remote file does not exists, `github.GithubException.UnknownObjectException` would raise
     file_content = repo.get_contents(path, ref=tag)
+    assert file_content.type == 'file'
+
+    time.sleep(1)  # in case that exceed the github access limit
 
     # save it to target directory
-    target_path = target_dir / file_content.name
-    with target_path.open('w+', encoding='utf8') as _f:
-        _f.write(file_content.decoded_content.decode('utf8'))
+    target_path = (target_dir / path).resolve()
+    target_path.parent.mkdir(parents=True, exist_ok=True)
+
+    with target_path.open('wb+') as _bin_io:
+        _bin_io.write(file_content.decoded_content)
 
     return target_path
     
+
+def _download_cpython_dir(path: str, tag: str, target_dir: Path) -> Path:
+    """ download all files recursively of a directory from cpython repository """
+    # if the remote file does not exists, `github.GithubException.UnknownObjectException` would raise
+    dir_content = repo.get_contents(path, ref=tag)
+    assert isinstance(dir_content, list)
+
+    time.sleep(1)  # in case that exceed the github access limit
+
+    # create path sub-directory within target directory
+    target_path = (target_dir / path).resolve()
+    target_path.mkdir(parents=True, exist_ok=True)
+
+    # walk the path
+    for _sub_content in dir_content:
+        if _sub_content.type == 'dir':
+            _download_cpython_dir(_sub_content.path, tag, target_dir)
+        else:  #  _sub_content.type == 'file'
+            _download_cpython_file(_sub_content.path, tag, target_dir)
+
+    return target_path
--- tests/test_dl.py
+++ tests/test_dl.py
@@ -7,7 +7,7 @@ from pprint import pprint
 import pytest
 
 # local imports
-from phy_cpydeps_update._downloader import _download_cpython_file
+from phy_cpydeps_update._downloader import _download_cpython_file, _download_cpython_dir
 
 
 # constants
@@ -16,10 +16,20 @@ BASE_DIR = TEST_DIR.parent
 TMP_DIR = BASE_DIR / 'tmp'
 
 
-def test_dl():
+@pytest.mark.skip()
+def test_dl_file():
     test_path_list = [
         'Parser/asdl_c.py'
     ]
 
     for test_path in test_path_list:
         _download_cpython_file(test_path, '3.12', TMP_DIR)
+
+
+def test_dl_dir():
+    test_path_list = [
+        'Tools/peg_generator'
+    ]
+
+    for test_path in test_path_list:
+        _download_cpython_dir(test_path, '3.12', TMP_DIR)
